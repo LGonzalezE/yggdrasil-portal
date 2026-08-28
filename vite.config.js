ed304d1f77ad127dd3cd90575138578eb6d2ac32
LGonzalezE <leonardo.gonzalez.escobar@gmail.com>
2026-08-28 15:53:07 -0500

    chore(vite): explicitly allow yggdrasil.local in allowedHosts

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,6 @@ export default defineConfig({
   server: {
     port: 3000,
     host: true,
-    allowedHosts: ['.yggdrasil.local', 'localhost']
+    allowedHosts: ['yggdrasil.local', '.yggdrasil.local', 'localhost']
   }
 });
